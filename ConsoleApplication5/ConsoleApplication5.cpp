4032747f19a0b10d5aec6dcf9c46b50e91e4eee7
ana.pistsowa@yandex.ru <anna.pistsova@yandex.ru>
2021-01-29 14:36:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ugol1;
+	int ugol2;
+	cout << "Vvedite znacheniya pervogo i vtorogo uglov: ";
+	cin >> ugol1;
+	cout << "\n";
+	cin >> ugol2;
+	if ((ugol1 + ugol2) < 180)
+	{
+		cout << "treugolnik suschestvuet\n";
+		if (ugol1 == 90 || ugol2 == 90 || (180 - ugol1 - ugol2) == 90)
+
+			cout << "treugolnik pryamougolniy\n";
+
+		else
+
+			cout << "treugolnik ne pryamougolniy\n";
+
+	}
+	else
+		cout << "treugolnik ne suschestvuet";
+}
